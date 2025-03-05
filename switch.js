0981d0ddaf9014254fcfc44022912337bbb88fef
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-03-06 01:15:14 +0300

    nested

---
+++ switch.js
@@ -0,0 +1,42 @@
+const number = "2";
+switch(number) {
+    case "1":
+        console.log("Sunday");
+        break;
+    case "2":
+        console.log("Monday");
+        break;
+    case "3":
+        console.log("Tuesday");
+        break;
+    case "4":
+        console.log("Wednesday");
+        break;
+    case "5":
+        console.log("Thursday");
+        break;
+    case "6":
+        console.log("Friday");
+        break;
+    case "7":
+        console.log("Saturday");
+        break;
+    default:
+        console.log ("Out of bounds exception");
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ tenary.js
@@ -0,0 +1,8 @@
+const number = 9;
+const no = number  ===1 ? "Sunday" : number === 2 ? "Monday" : number === 3 ? "Tuesday" : number === 4 ? "Wednesday" : number  === 5 ? "Thursday" : number === 6 ? "Friday" : number  === 7 ? "Saturday" : "Out of bounds exception";
+console.log(no);
+
+
+const age =75;
+const agenob = age <=2 ? "toddler" : age <=13 ? "teenager" : age <=18 ? "adult" : age <=30 ? "youth" : age <= 65 ? "senior citizen" : "dead";
+console.log(agenob);
